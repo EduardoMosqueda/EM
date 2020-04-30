a5af342c81adb0d3c615aee0e467d8bf04f7fc3d
EduardoM <eduardo7mosqueda@gmail.com>
2020-04-30 18:45:18 -0400

    Auto-generated commit

--- angular-particle/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './interfaces';
-export * from './utils';
-export { CanvasManager } from './CanvasManager';
-export { ParticlesManager } from './ParticlesManager';
-export { Particle } from './Particle';
-export { ParticleInteraction } from './ParticleInteraction';
