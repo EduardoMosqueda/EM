cc97f6d6666c6a4598d7d65813cc2b46b492a74e
unknown <eduardo7mosqueda@gmail.com>
2022-03-04 11:39:47 -0400

    Auto-generated commit

--- angular-particle/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './particles.component';
-export * from './particles.directive';
-export declare class ParticlesModule {
-}
--- angular-particle/particles.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { IParams } from './lib/index';
-export declare class ParticlesComponent {
-    width: number;
-    height: number;
-    params: IParams;
-    style: object;
-    constructor();
-}
